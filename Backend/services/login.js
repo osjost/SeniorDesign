35fe032ef1090b4e2a6fe24efebac02f0d7fb218
osjost <73126165+osjost@users.noreply.github.com>
2023-11-24 13:54:53 -0700

    adds fix

--- Backend/services/login.js
+++ Backend/services/login.js
@@ -6,25 +6,25 @@ const saltRounds = 10;
 const secret = "1z98AJf901JZAa"
 
 
-async function authenticate(username, password){
-    // get password hash from database
-    // In the below code, result will be a [ { password_hash: '1kjlasjdf' } ]
+async function authenticate(username, password) {
+    // Get password hash and user_id from the database
     const result = await db.query(
-        `SELECT password_hash FROM login WHERE username = ?;`,
+        `SELECT password_hash, user_id FROM login WHERE username = ?;`,
         [username]
-      );
+    );
 
-      if (result.length > 0) {
-        // verify it matches, if it does return JWT
+    if (result.length > 0) {
+        const user = result[0]; // user now contains { password_hash: '...', user_id: '...' }
+
         return new Promise((resolve, reject) => {
-            bcrypt.compare(password, result[0].password_hash, function(err, compareResult) {
+            bcrypt.compare(password, user.password_hash, function(err, compareResult) {
                 if (err) {
                     // Handle bcrypt error
                     reject(err);
                 } else if (compareResult) {
                     // Passwords match, create JWT
-                    const jwtString = jwt.encode({ username: username }, secret);
-                    resolve(jwtString);
+                    const jwtString = jwt.encode({ username: username, userId: user.user_id }, secret);
+                    resolve({ jwt: jwtString, user_id: user.user_id }); // Return the JWT and the user object
                 } else {
                     // Passwords do not match
                     reject(new Error('Authentication failed'));
@@ -35,6 +35,7 @@ async function authenticate(username, password){
         throw new Error('User not found');
     }
 }
+
 
 module.exports = {
     authenticate
