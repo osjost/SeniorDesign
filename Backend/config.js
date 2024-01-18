deeabe89ce945665f26ec1ab462d62123e4395bc
osjost <73126165+osjost@users.noreply.github.com>
2024-01-18 12:29:24 -0700

    aws

--- Backend/config.js
+++ Backend/config.js
@@ -2,8 +2,8 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "localhost",
-    user: "database-test1.chaq6oaiibfk.us-west-1.rds.amazonaws.com",
+    host: "database-test1.chaq6oaiibfk.us-west-1.rds.amazonaws.com",
+    user: "admin",
     password: "bdYND8yKgub4z3",
     database: "cytocheck",
     port: '3306'
