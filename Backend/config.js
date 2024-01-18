2f319e4d80a275726a642f7e5e4fbe243b51e1ab
osjost <73126165+osjost@users.noreply.github.com>
2024-01-18 12:26:40 -0700

    aws changes

--- Backend/config.js
+++ Backend/config.js
@@ -1,13 +1,27 @@
+// for the aws released version:
 const config = {
-    db: {
-      /* don't expose password or any sensitive info, done only for demo */
-      host: "localhost",
-      user: "root",
-      password: "X4eeqle35/",
-      database: "cytocheck",
-      port: '3306'
-    },
-  };
-  module.exports = config;
+  db: {
+    /* don't expose password or any sensitive info, done only for demo */
+    host: "localhost",
+    user: "database-test1.chaq6oaiibfk.us-west-1.rds.amazonaws.com",
+    password: "bdYND8yKgub4z3",
+    database: "cytocheck",
+    port: '3306'
+  },
+};
+module.exports = config;
+
+// below is for local host testing, JUST COMMENT THE ABOVE OUT DO NOT DELETE IT PLS
+// const config = {
+//     db: {
+//       /* don't expose password or any sensitive info, done only for demo */
+//       host: "localhost",
+//       user: "root",
+//       password: "X4eeqle35/",
+//       database: "cytocheck",
+//       port: '3306'
+//     },
+//   };
+//   module.exports = config;
 
 
