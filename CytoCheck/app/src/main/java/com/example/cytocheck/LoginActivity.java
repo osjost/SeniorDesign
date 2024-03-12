6de884ed42aec9515bcf3a852e7bd776eca8376a
Drolio <adriancorey@arizona.edu>
2024-03-12 16:55:42 -0700

    jjjjj

--- CytoCheck/app/src/main/java/com/example/cytocheck/LoginActivity.java
+++ CytoCheck/app/src/main/java/com/example/cytocheck/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.cytocheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.util.Log;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 
 //Import API stuff
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import org.json.JSONObject;
 import org.json.JSONException;
 
@@ -28,6 +39,32 @@ public class LoginActivity extends AppCompatActivity {
     private String token = "";
     private String userID = "";
 
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                }
+            });
+
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -134,4 +171,34 @@ public class LoginActivity extends AppCompatActivity {
             }
         }); //end of submit on click listener
     } //end of onCreate
+
+    @Override
+        protected void onResume() {
+            super.onResume();
+            Log.w("messaging", "here");
+    // Your condition to ensure you don't ask for permission unnecessarily
+            if (true) {
+                askNotificationPermission();
+            }
+
+
+    //        this actually gets the users token, assuming that they enter yes. we have to add more UI elements to account for this:
+            FirebaseMessaging.getInstance().getToken()
+                    .addOnCompleteListener(new OnCompleteListener<String>() {
+                        @Override
+                        public void onComplete(@NonNull Task<String> task) {
+                            if (!task.isSuccessful()) {
+                                Log.w("messaging", "Fetching FCM registration token failed", task.getException());
+                                return;
+                            }
+
+                            // Get new FCM registration token
+                            String token = task.getResult();
+
+                            // Log and toast
+                            Log.d("messaging", token);
+                            // Send the token to your server to keep it updated
+                        }
+                    });
+        }
 } //end of main activity
