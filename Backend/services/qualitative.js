9f6ce7913ffcd4b4ddcf47ab7c00cd268b910ba3
osjost <73126165+osjost@users.noreply.github.com>
2024-03-21 12:59:54 -0700

    Update qualitative.js

--- Backend/services/qualitative.js
+++ Backend/services/qualitative.js
@@ -20,6 +20,46 @@ async function create(qualitative){
     if (result.affectedRows) {
       message = 'qualitative data added succesfully';
     }
+
+    // detect if we want to send a notification and send if yes
+    const rows = await db.query(
+      `SELECT * 
+       FROM qualitative_data 
+       WHERE user_id = ? 
+       ORDER BY created_at DESC 
+       LIMIT 1`,
+      [qualitative.user_id]
+    );
+
+    if (rows.length != 0) {
+      entry = rows[0]
+      const conditionsMet = 
+        Math.abs(entry.nausea - qualitative.nausea) > 3 ||
+        Math.abs(entry.fatigue - qualitative.fatigue) > 3 ||
+        Math.abs(entry.pain - qualitative.pain) > 3 ||
+        Math.abs(entry.rash - qualitative.rash) > 3;
+
+      if (conditionsMet) {
+          const rows = await db.query(
+            `SELECT * FROM provider_patient_associations WHERE patient_id = ?;`,
+            [reading.user_id]
+        );
+
+        for (const provider of rows) {
+          // get fcc of entry
+          let fccRows = fcc.get(provider.provider_id)
+
+          for (const fcc of fccRows) {
+            smsService.sendFirebaseNotification(fcc, "Emergency with user " + reading.user_id, "Threshold breach detected") 
+          }
+        }
+
+      }
+    }
+    
+
+
+
   
     return {message};
   }
